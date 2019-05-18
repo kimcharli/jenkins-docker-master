d18f9800d4e1400af931431ff6278e409964dd14
Charlie (Chang-Hyun) Kim <ckim@juniper.net>
2019-05-17 21:37:45 -0400

    first commit

---
+++ run_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+for i in 1 2 3 4 5 6 7 8 9 0
+do
+  echo ==== ${i}
+  curl -s jenkins:8080  | grep "Authentication required" && exit 0
+#  curl jenkins:8080 -D -
+  sleep 10
+done
+
+exit 1
+
+
