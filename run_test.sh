88277380a76a40dd4008f747da128d68dce6b032
Charlie (Chang-Hyun) Kim <ckim@juniper.net>
2019-05-18 11:46:20 -0400

    increasing wait time for docker hub test

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for i in 1 2 3 4 5 6 7 8 9 0
+for i in 1 2 3 4 5 6 7 8 9 10
 do
   echo ==== ${i}
 #  curl -s jenkins:8080  | grep "Authentication required" && exit 0
   curl jenkins:8080 -D -
-  sleep 10
+  sleep 30
 done
 
 echo timeout
