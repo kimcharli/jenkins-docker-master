fd790a41b68af2cb25ca61781c7f91a24b993ba8
Charlie (Chang-Hyun) Kim <ckim@juniper.net>
2019-05-17 22:58:39 -0400

    add debugging

--- run_test.sh
+++ run_test.sh
@@ -3,8 +3,8 @@
 for i in 1 2 3 4 5 6 7 8 9 0
 do
   echo ==== ${i}
-  curl -s jenkins:8080  | grep "Authentication required" && exit 0
-#  curl jenkins:8080 -D -
+#  curl -s jenkins:8080  | grep "Authentication required" && exit 0
+  curl jenkins:8080 -D -
   sleep 10
 done
 
